d6b55d36e8f49652088a881a1dceb424d3147e3c
tshelovecpayc-stack <tshelovecpayc@gmail.com>
2025-10-01 11:07:23 +0300

    Add files via upload

---
+++ laba1.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main() {
+    printf("        /\\\n");
+    printf("       /  \\\n");
+    printf("      /    \\\n");
+    printf("     /______\\\n");
+    printf("     |      |\n");
+    printf("     |      |\n");
+    printf("     |  __  |\n");
+    printf("     | |  | |\n");
+    printf("     | |__| |\n");
+    printf("     |      |\n");
+    printf("     |______|\n");
+
+    return 0;
+}
