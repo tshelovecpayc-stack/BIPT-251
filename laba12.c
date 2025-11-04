a26e041f1678fcfb9f0966e6b443d096ca05aa37
tshelovecpayc-stack <tshelovecpayc@gmail.com>
2025-11-04 20:33:34 +0300

    Update laba12.c

--- laba12.c
+++ laba12.c
@@ -5,7 +5,7 @@
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // Создаем переменные каждого типа
+    // РЎРѕР·РґР°РµРј РїРµСЂРµРјРµРЅРЅС‹Рµ РєР°Р¶РґРѕРіРѕ С‚РёРїР°
     char c;
     short int si;
     unsigned short int usi;
@@ -18,18 +18,18 @@ int main() {
     double d;
     signed int si2;
 
-    // Вычисляем размеры
-    printf("1) char: %ld байт(а)\n", GET_SIZE(c));
-    printf("2) short int: %ld байт(а)\n", GET_SIZE(si));
-    printf("3) unsigned short int: %ld байт(а)\n", GET_SIZE(usi));
-    printf("4) int: %ld байт(а)\n", GET_SIZE(i));
-    printf("5) unsigned char: %ld байт(а)\n", GET_SIZE(uc));
-    printf("6) wchar_t: %ld байт(а)\n", GET_SIZE(wc));
-    printf("7) long long int: %ld байт(а)\n", GET_SIZE(lli));
-    printf("8) size_t: %ld байт(а)\n", GET_SIZE(st));
-    printf("9) float: %ld байт(а)\n", GET_SIZE(f));
-    printf("10) double: %ld байт(а)\n", GET_SIZE(d));
-    printf("11) signed int: %ld байт(а)\n", GET_SIZE(si2));
+    // Р’С‹С‡РёСЃР»СЏРµРј СЂР°Р·РјРµСЂС‹
+    printf("1) char: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(c));
+    printf("2) short int: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(si));
+    printf("3) unsigned short int: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(usi));
+    printf("4) int: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(i));
+    printf("5) unsigned char: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(uc));
+    printf("6) wchar_t: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(wc));
+    printf("7) long long int: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(lli));
+    printf("8) size_t: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(st));
+    printf("9) float: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(f));
+    printf("10) double: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(d));
+    printf("11) signed int: %ld Р±Р°Р№С‚(Р°)\n", GET_SIZE(si2));
 
     return 0;
 }
