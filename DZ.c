b6060b22ac45d0ae7983ceae645ddc19b93326e9
tshelovecpayc-stack <tshelovecpayc@gmail.com>
2025-11-04 18:02:21 +0300

    Add files via upload

---
+++ DZ.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int N;
+    double sum = 0.0;
+
+    printf("¬ведите число N: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        sum += 1.0 / i;
+    }
+
+    printf("—умма 1 + 1/2 + 1/3 + ... + 1/%d = %lf\n", N, sum);
+
+    return 0;
+}
