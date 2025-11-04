4c712caafed7bd41d1df9d9c6cf4044d448e5308
tshelovecpayc-stack <tshelovecpayc@gmail.com>
2025-11-04 17:06:01 +0300

    Update laba!4.c

--- laba!4.c
+++ laba!4.c
@@ -1,43 +1,20 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 #include <locale.h>
+#include <stdio.h>
 
-int main() {
-    setlocale(LC_ALL, "Russian");
+    int main() {
+        setlocale(LC_ALL, "Russian");
+        int A, B, C;
 
-    int powerA, powerB, powerC;  // уровни силы героев
-    int canActivate;             // флаг активации способности
+        printf("Введите A, B, C: ");
+        scanf("%d %d %d", &A, &B, &C);
 
-    printf("АКТИВАЦИЯ СПОСОБНОСТИ 'ТРОЙНОЙ УДАР'\n\n");
+        if (A % 3 == 0 && B % 3 == 0 && C % 3 == 0) {
+            printf("Портал открыт!\n");
+        }
+        else {
+            printf("Портал закрыт!\n");
+        }
 
-    // Ввод уровней силы героев
-    printf("Введите уровень силы героя A: ");
-    scanf("%d", &powerA);
-
-    printf("Введите уровень силы героя B: ");
-    scanf("%d", &powerB);
-
-    printf("Введите уровень силы героя C: ");
-    scanf("%d", &powerC);
-
-    // Проверка условия активации
-    canActivate = (powerA % 3 == 0) && (powerB % 3 == 0) && (powerC % 3 == 0);
-
-    // Вывод результатов
-    printf("\nРЕЗУЛЬТАТ ПРОВЕРКИ\n");
-    printf("Герой A: уровень силы = %d, кратен 3: %s\n",
-        powerA, (powerA % 3 == 0) ? "ДА" : "НЕТ");
-    printf("Герой B: уровень силы = %d, кратен 3: %s\n",
-        powerB, (powerB % 3 == 0) ? "ДА" : "НЕТ");
-    printf("Герой C: уровень силы = %d, кратен 3: %s\n",
-        powerC, (powerC % 3 == 0) ? "ДА" : "НЕТ");
-
-    if (canActivate) {
-        printf("\nСПОСОБНОСТЬ АКТИВИРОВАНА! Тройной удар готов к использованию!\n");
+        return 0;
     }
-    else {
-        printf("\nСПОСОБНОСТЬ НЕДОСТУПНА! Не все герои удовлетворяют условию.\n");
-    }
-
-    return 0;
-}
